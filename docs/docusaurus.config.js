c9b50dd07ed76a248155c491f74e80057112eb85
Ruben van der Linde <rubenvdlinde@gmail.com>
2026-05-29 10:02:53 +0200

    docs: retarget Docusaurus GitHub links to Codeberg and relabel navbar

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -60,7 +60,7 @@ const config = createConfig({
              plus the standard node_modules bucket. */
           exclude: ['**/node_modules/**', 'src/**'],
           sidebarPath: require.resolve('./sidebars.js'),
-          editUrl: 'https://github.com/ConductionNL/nldesign/tree/main/docs/',
+          editUrl: 'https://codeberg.org/Conduction/nldesign/src/branch/main/docs/',
         },
         blog: false,
         theme: {
@@ -87,8 +87,8 @@ const config = createConfig({
         label: 'Documentation',
       },
       {
-        href: 'https://github.com/ConductionNL/nldesign',
-        label: 'GitHub',
+        href: 'https://codeberg.org/Conduction/nldesign',
+        label: 'Codeberg',
         position: 'right',
       },
       { type: 'localeDropdown', position: 'right' },
